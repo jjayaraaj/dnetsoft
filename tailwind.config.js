05e7031f1f1ffa2c88e990fcd0aa7de40ef7f988
jjayaraaj <jjayaraaj@gmail.com>
2022-03-12 16:23:30 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dnetblue: {
+          dark: "#0a1d47",
+          default: "#385e97",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
